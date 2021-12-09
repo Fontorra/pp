a31b5772bd5560034904bc02bd691ee49df8ab3d
Fontorra <95175825+Fontorra@users.noreply.github.com>
2021-12-09 22:31:02 +0300

    Add files via upload

---
+++ server.py
@@ -0,0 +1,59 @@
+from flask import Flask
+from flask import render_template, redirect, url_for
+from flask import request
+import datetime
+import blockChain
+import psycopg2
+app = Flask(__name__)
+
+conn = psycopg2.connect(database="postgres", user="postgres", host="localhost", password="1234")
+cur = conn.cursor()
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    # print(request.method )
+    print(3)
+    if request.method == 'POST':
+        login = request.form['login']
+        if len(login) < 1:
+            return redirect(url_for('index'))
+        try:
+            password = request.form['password']
+        except Exception:
+            password = False
+        cur.execute('SELECT "Password" FROM public."User" WHERE "Login" = \'%s\'' % login)
+        pswd = cur.fetchone()
+        print(password, login)
+        if pswd == password:
+            cur.execute('SELECT position FROM public."User" WHERE "Login" = \'%s\'' % login)
+            position = cur.fetchone()
+            cur.execute('SELECT id FROM public."User" WHERE "Login" = \'%s\'' % login)
+            id = cur.fetchone()
+        return render_template('conspect.html')
+    return render_template('index.html')
+
+@app.route('/conspect', methods = ['GET', 'POST'])
+def conspect():
+    position = 1
+    id = 1
+    print(id)
+    if request.method == 'POST':
+        print(2)
+        text = request.form['text']
+        print(text)
+        now = datetime.datetime.now()
+        if len(text) < 1:
+            return redirect('conspect')
+        if position == 1:
+            cur.execute('INSERT INTO public."Meta_info" (note_supervisor, id_supervisor, date) VALUES (\'%s\', %s, %s)' % (text, id, now))
+        else:
+            cur.execute('INSERT INTO public."Meta_info" (note_student_1, student, date) VALUES (\'%s\', %s)' % (text, id, now))
+    return redirect('conspect')
+@app.route('/valid', methods = ['GET', 'POST'])
+def valid():
+    return 1
+if __name__ == '__main__':
+    app.run(debug=True)
+conn.commit()
+cur.close()
+conn.close()
